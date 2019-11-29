7d6415b38877fa0ef833bba0f542a5cff12111be
joshuaasare <joshuaasare29@gmail.com>
2019-11-29 17:21:19 +0000

    own cors instance

--- src/features/Login/_helpers/locationService.js
+++ src/features/Login/_helpers/locationService.js
@@ -3,7 +3,7 @@ import { constants } from '../../_shared/constants';
 
 export async function getPlacesFromSearchKey(searchKey: string): Array<Object> {
   try {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const proxyUrl = 'https://coe-cors-anywhere.herokuapp.com/';
     const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${searchKey}&key=${constants.maps.API_KEY}`;
     const resp = await axios.get(`${proxyUrl}${url}`);
     const options = await convertPlacesToOptionsForUi(resp.data.predictions);
@@ -22,7 +22,7 @@ export async function convertPlacesToOptionsForUi(data: Array) {
 
 export async function getLocationDetails(locationId) {
   try {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const proxyUrl = 'https://coe-cors-anywhere.herokuapp.com/';
     const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${locationId}&key=${constants.maps.API_KEY}`;
     const resp = await axios.get(`${proxyUrl}${url}`);
     const result = resp.data.result;
