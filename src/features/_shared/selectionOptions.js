dd4b3a2bad7bd8d6ad8709decbcb19597fa2cdee
joshuaasare <joshuaasare29@gmail.com>
2020-02-06 10:33:04 +0000

    added second years

--- src/features/_shared/selectionOptions.js
+++ src/features/_shared/selectionOptions.js
@@ -42,7 +42,7 @@ export const selectionOptions = {
   ],
   YEAR_OF_STUDY: [
     // { text: '1st Year', value: '1' },
-    // { text: '2nd Year', value: '2' },
+    { text: '2nd Year', value: '2' },
     { text: '3rd Year', value: '3' },
     // { text: '4th Year', value: '4' },
     // { text: '5th Year', value: '5' },
